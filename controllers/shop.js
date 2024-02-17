d4ab57ee7dc51d6cfc412152731201fc789d89ec
phuc_le_123 <phuc.le@adurolife.com>
2024-02-17 20:32:55 +0700

    Loading Product Details

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,6 +14,9 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
   console.log(productId);
+  Product.findById(productId, (product) => {
+    console.log(product);
+  });
   res.redirect("/");
 };
 
