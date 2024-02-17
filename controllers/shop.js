1a1665c08bc6ef9d9f5f38a6b1ff646adb2f14ca
phuc_le_123 <phuc.le@adurolife.com>
2024-02-17 21:40:09 +0700

    Passing Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,6 +41,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log(productId);
+  res.redirect("/cart");
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
