cf9df29793579f30e3440ac353cfcb8918d41adc
phuc_le_123 <phuc.le@adurolife.com>
2024-02-17 19:03:20 +0700

    Registering the Routes

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,4 +1,4 @@
 exports.get404 = (req, res, next) => {
-    //res.sendFile(path.join(__dirname, "views", "404.html"));
-    res.render('404', { pageTitle: 'Page Not Found', path: '' })
+  //res.sendFile(path.join(__dirname, "views", "404.html"));
+  res.render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
