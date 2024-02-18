3872132cddb43de70d815f61a4a83caf71fd95de
phuc_le_123 <phuc.le@adurolife.com>
2024-02-18 10:19:10 +0700

    Adding a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   //res.sendFile(path.join(rootDir, "views", "shop.html"));
@@ -43,7 +44,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  console.log(productId);
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, product.price);
+  });
   res.redirect("/cart");
 };
 
