e2abb9bf282d29c84833ea020a951cee6aebf053
phuc_le_123 <phuc.le@adurolife.com>
2024-02-19 21:38:21 +0700

    Inserting data into the database

--- models/product.js
+++ models/product.js
@@ -10,7 +10,12 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
 
   static fetchAll() {
     return db.execute("SELECT * FROM products");
