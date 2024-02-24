4d70d5d3f305c6dcba3480a11e97c22043a22ca2
tomdwan123 <tomdwanno1@gmail.com>
2024-02-24 21:22:47 +0700

    Creating and Fetching a Cart

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then((result) => {
     return User.findByPk(1);
   })
@@ -65,8 +65,11 @@ sequelize
 
     return user;
   })
-  .then((result) => {
-    //console.log(result);
+  .then((user) => {
+    //console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
