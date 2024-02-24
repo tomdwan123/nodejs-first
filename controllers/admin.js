6bbe71267dcef4bb990dd2ccb5a07876cc2a8dee
tomdwan123 <tomdwanno1@gmail.com>
2024-02-24 20:42:20 +0700

    Fetching Related Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -44,8 +44,11 @@ exports.getEditProduct = (req, res, next) => {
   }
 
   const productId = req.params.productId;
-  Product.findByPk(productId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: productId } })
+    //Product.findByPk(productId)
+    .then((products) => {
+      const product = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -95,7 +98,8 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
