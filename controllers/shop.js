86df282df6586fae54378996ab38853d955408d4
tomdwan123 <tomdwanno1@gmail.com>
2024-02-29 17:19:54 +0700

    Resetting the cart and fetching and outputting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -136,10 +136,17 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      console.log(orders);
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
